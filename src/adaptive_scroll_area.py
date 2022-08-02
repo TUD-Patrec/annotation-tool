9f3c7e95b982468b3053a9287adb3706363cc7c5
Raphael Spiekermann <rs15061998@gmail.com>
2022-08-02 11:06:01 +0200

    Reworked the annotation-dialog. Added better working scroll area.

---
+++ src/adaptive_scroll_area.py
@@ -0,0 +1,49 @@
+import PyQt5.QtWidgets as qtw
+import PyQt5.QtCore as qtc
+
+
+class QAdaptiveScrollArea(qtw.QWidget):
+    def __init__(self, parent=None):
+        super(QAdaptiveScrollArea, self).__init__(parent=parent)
+        self.items = []
+        self.initUI()
+        
+    def initUI(self):
+        self.layout = qtw.QHBoxLayout(self)
+        self.scrollArea = qtw.QScrollArea(self)
+        self.scrollArea.setWidgetResizable(True)
+        self.scrollAreaWidgetContents = qtw.QWidget()
+        self.vBoxLayout = qtw.QVBoxLayout(self.scrollAreaWidgetContents)
+        self.vBoxLayout.setAlignment(qtc.Qt.AlignLeft)
+        self.scrollArea.setWidget(self.scrollAreaWidgetContents)
+        self.layout.addWidget(self.scrollArea)
+        
+        #self.scrollArea.setVerticalScrollBarPolicy(qtc.Qt.ScrollBarAlwaysOn)
+        self.scrollArea.setHorizontalScrollBarPolicy(qtc.Qt.ScrollBarAlwaysOff)
+        
+        for item in self.items:
+            self.vBoxLayout.addWidget(item)
+    
+    def updateUI(self):         
+        for item in self.items:
+            item.setParent(self)
+        
+        self.scrollAreaWidgetContents = qtw.QWidget()
+        self.vBoxLayout = qtw.QVBoxLayout(self.scrollAreaWidgetContents)
+        self.vBoxLayout.setAlignment(qtc.Qt.AlignLeft)
+        self.scrollArea.setWidget(self.scrollAreaWidgetContents)
+        
+        for item in self.items:
+            self.vBoxLayout.addWidget(item)
+        
+            
+    def clear(self):
+        for item in self.items:
+            item.setParent(None)
+        self.items = []
+        self.updateUI()
+        
+    def addItem(self, item):
+        self.items.append(item)
+        self.updateUI()
+        
