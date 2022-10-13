b5fb6610e57b37c65f121645db4446e6faecaf23
Raphael <raphael.spiekermann@tu-dortmund.de>
2022-10-13 19:13:47 +0200

    added resource_path

--- main.py
+++ main.py
@@ -20,6 +20,16 @@ def get_application_path():
         raise RuntimeError("Could not get the path of this script")
     return application_path
 
+def resource_path(relative_path):
+    """ Get absolute path to resource, works for dev and for PyInstaller """
+    try:
+        # PyInstaller creates a temp folder and stores path in _MEIPASS
+        base_path = sys._MEIPASS
+    except Exception:
+        base_path = os.path.abspath(".")
+
+    return os.path.join(base_path, relative_path)
+
 
 def enable_high_dpi_scaling():
 
