f5256004bc15aeb0aee842732b1580776d82525a
Raphael Spiekermann <raphael.spiekermann@tu-dortmund.de>
2023-05-30 15:04:12 +0200

    fix(decorators.py): Remove unused functions

--- annotation_tool/utility/decorators.py
+++ annotation_tool/utility/decorators.py
@@ -1,6 +1,3 @@
-from contextlib import suppress
-from functools import wraps
-import inspect
 import typing
 
 
@@ -63,58 +60,6 @@ def returns(rtype: typing.Type) -> typing.Callable:
     return check_returns
 
 
-# @accepts(int, (int,float))
-# @returns((int,float))
-# def func(arg1, arg2):
-#     return arg1 * arg2
-
-
-def enforce_types(callable):
-    spec = inspect.getfullargspec(callable)
-
-    def check_types(*args, **kwargs):
-        parameters = dict(zip(spec.args, args))
-        parameters.update(kwargs)
-        for name, value in parameters.items():
-            with suppress(KeyError):  # Assume un-annotated parameters can be any type
-                type_hint = spec.annotations[name]
-                if isinstance(type_hint, typing._SpecialForm):
-                    # No check for typing.Any, typing.Union,
-                    # typing.ClassVar (without parameters)
-                    continue
-                try:
-                    actual_type = type_hint.__origin__
-                except AttributeError:
-                    # In case of non-typing types (such as <class 'int'>, for instance)
-                    actual_type = type_hint
-                # In Python 3.8 one would replace the try/except with
-                # actual_type = typing.get_origin(type_hint) or type_hint
-                if isinstance(actual_type, typing._SpecialForm):
-                    # case of typing.Union[…] or typing.ClassVar[…]
-                    actual_type = type_hint.__args__
-
-                if not isinstance(value, actual_type):
-                    raise TypeError(
-                        "Unexpected type for '{}' (expected {} but found {})".format(
-                            name, type_hint, type(value)
-                        )
-                    )
-
-    def decorate(func):
-        @wraps(func)
-        def wrapper(*args, **kwargs):
-            check_types(*args, **kwargs)
-            return func(*args, **kwargs)
-
-        return wrapper
-
-    if inspect.isclass(callable):
-        callable.__init__ = decorate(callable.__init__)
-        return callable
-
-    return decorate(callable)
-
-
 class Singleton:
     """
     A non-thread-safe helper class to ease implementing singletons.
@@ -133,6 +78,7 @@ class Singleton:
 
     def __init__(self, decorated):
         self._decorated = decorated
+        self._instance = None
 
     def instance(self):
         """
@@ -141,11 +87,9 @@ class Singleton:
         On all subsequent calls, the already created instance is returned.
 
         """
-        try:
-            return self._instance
-        except AttributeError:
+        if self._instance is None:
             self._instance = self._decorated()
-            return self._instance
+        return self._instance
 
     def __call__(self):
         raise TypeError("Singletons must be accessed through `instance()`.")
